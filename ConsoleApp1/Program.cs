d7357a8ba92c0c55355c5ad34ce7b8af1b015fe6
kanadeiar <kanadeiar@gmail.com>
2020-05-25 16:01:34 +0300

    new text

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Привет мир!");
+            Console.WriteLine("Нажмите любую кнопку...");
             Console.ReadKey();
         }
     }
