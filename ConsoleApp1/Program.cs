0b34123ae1d5ed3c6fc4fa7288a67bd2ccf0ae87
kanadeiar <kanadeiar@gmail.com>
2020-05-25 16:03:42 +0300

    delete text

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Привет мир!");
-            Console.WriteLine("Нажмите любую кнопку...");
             Console.ReadKey();
         }
     }
