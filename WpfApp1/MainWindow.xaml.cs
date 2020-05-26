e6ed647f336662504dbddb791e1a3e7eedf582ab
kanadeiar <kanadeiar@gmail.com>
2020-05-26 12:39:57 +0300

    clear project

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Xps.Packaging;
+using Microsoft.Win32;
 
 namespace WpfApp1
 {
@@ -20,17 +14,13 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
-
-        private void Press_OnClick(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Properties["Test"] = true;
-            if ((bool) Application.Current.Properties["Test"])
-                MessageBox.Show("Есть!");
-        }
     }
 }
