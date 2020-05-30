5ebb76eba4b0f0c4bc837f741842ed178a816a36
kanadeiar <kanadeiar@gmail.com>
2020-05-30 18:30:35 +0300

    add visual

---
+++ WpfApp1/CustomVisualFrameworkElement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public class CustVisualFrameworkElement : FrameworkElement
+    {
+        private VisualCollection collection; //коллекция всех элементов
+        public CustVisualFrameworkElement()
+        {
+            collection = new VisualCollection(this) //заполнить коллекцию квадартом и кругом
+            {
+                AddRect(),
+                AddCircle(),
+            };
+        }
+        private Visual AddCircle()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual(); 
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen()) //объект для создания нового содержимого
+            {
+                drawingContext.DrawEllipse(Brushes.DarkBlue, null, new Point(80,80), 40,50); //создать круг и нарисовать его
+            }
+            return drawingVisual;
+        }
+        private Visual AddRect()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen()) //объект для создания нового содержимого
+            {
+                drawingContext.DrawRectangle(Brushes.Tomato, null, new Rect(new Point(150,100), new Size(300,100))); //создать квадрат и нарисовать его
+            }
+            return drawingVisual;
+        }
+        protected override int VisualChildrenCount => collection.Count;
+        protected override Visual GetVisualChild(int index)
+        {
+            if (index < 0 || index >= collection.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return collection[index]; //коллекция из графических элементов
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Text;
 using System.Windows;
 using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using System.Windows.Xps.Packaging;
 using Microsoft.Win32;
 
@@ -20,7 +23,22 @@ namespace WpfApp1
             InitializeComponent();
 
         }
-
-
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            //создать форматированный текст
+            FormattedText text = new FormattedText("Визуализация", new CultureInfo("ru-ru"),FlowDirection.LeftToRight,new Typeface(this.FontFamily,FontStyles.Italic,FontWeights.DemiBold,FontStretches.UltraExpanded),30,Brushes.DarkGreen,null,VisualTreeHelper.GetDpi(this).PixelsPerDip);
+            //создать контекст визуализации
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                //методы DrawingContext для визуализации данных
+                drawingContext.DrawRoundedRectangle(Brushes.Yellow, new Pen(Brushes.Black, 5), new Rect(5,5,400,100), 20, 20);
+                drawingContext.DrawText(text, new Point(20,20));
+            }
+            //битовое изображение из визуального контекста
+            RenderTargetBitmap bitmap = new RenderTargetBitmap(500,300,100,100,PixelFormats.Pbgra32);
+            bitmap.Render(drawingVisual);
+            myImage.Source = bitmap;
+        }
     }
 }
