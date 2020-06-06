379fbe12c61ad314b3118fb388108da57d9988ff
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-06 09:36:33 +0300

    obs model and list work

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Xps.Packaging;
 using Microsoft.Win32;
+using WpfApp1.Models;
 
 namespace WpfApp1
 {
@@ -14,13 +16,42 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        
+        readonly IList<Inventory> cars = new ObservableCollection<Inventory>(); //наблюдаемая коллекция
         public MainWindow()
         {
             InitializeComponent();
-
+            cars.Add(new Inventory
+            {
+                CarId = 1,
+                Make = "УАЗ",
+                Color = "Белый",
+                PetName = "Буханка",
+            });
+            cars.Add(new Inventory
+            {
+                CarId = 2,
+                Make = "ЗАЗ",
+                Color = "Коричневый",
+                PetName = "Большая"
+            });
+            comboBoxCars.ItemsSource = cars;
         }
 
+        private void ButtonChangeColor_OnClick(object sender, RoutedEventArgs e)
+        {
+            cars.First(x => x.CarId == ((Inventory) comboBoxCars.SelectedItem)?.CarId).Color = "Pink";
+        }
+
+        private void buttonAddCar_Click(object sender, RoutedEventArgs e)
+        {
+            var maxCount = cars?.Max(x => x.CarId) ?? 0;
+            cars.Add(new Inventory
+            {
+                CarId = maxCount + 1,
+                Color = "Желтый",
+                Make = "ВАЗ",
+                PetName = "Копейка",
+            });
+        }
     }
 }
--- WpfApp1/Models/Inventory.cs
+++ WpfApp1/Models/Inventory.cs
@@ -1,16 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfApp1.Models
 {
-    class Inventory
+    class Inventory : INotifyPropertyChanged //наблюдаемая модель
     {
-        public int CarId { get; set; }
-        public string Make { get; set; }
-        public string Color { get; set; }
-        public string PetName { get; set; }
+        private int carId;
+        public int CarId
+        {
+            get => carId;
+            set
+            {
+                if (value == carId) return;
+                carId = value;
+                //передача названия свойства значение которого изменилось
+                OnPropertyChanged(nameof(CarId));
+            }
+        }
+        private string make;
+        public string Make
+        {
+            get => make;
+            set
+            {
+                if (value == make) return;
+                make = value;
+                OnPropertyChanged(nameof(Make));
+            }
+        }
+        private string color;
+        public string Color
+        {
+            get => color;
+            set
+            {
+                if (value == color) return;
+                color = value;
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+        private string petName;
+        public string PetName
+        {
+            get => petName;
+            set
+            {
+                if (value == petName) return;
+                petName = value;
+                OnPropertyChanged(nameof(PetName));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary> Вспомогательный метод индицирования события изменения свойства </summary>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            //индицирование события со строкой, указывающей свойство, которое было изменено и нуждается в обновлении
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            //обновление всех привязанных свойств объекта
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
+        }
     }
 }
