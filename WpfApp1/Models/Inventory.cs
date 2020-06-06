6e12451259c9dd4536434f54d8cc05605d79ec8d
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-06 11:30:29 +0300

    add Notify

--- WpfApp1/Models/Inventory.cs
+++ WpfApp1/Models/Inventory.cs
@@ -8,80 +8,89 @@ using System.Threading.Tasks;
 
 namespace WpfApp1.Models
 {
-    class Inventory : INotifyPropertyChanged //наблюдаемая модель
+    class Inventory : INotifyPropertyChanged
     {
-        private int carId;
-        public int CarId
-        {
-            get => carId;
-            set
-            {
-                if (value == carId) return;
-                carId = value;
-                //передача названия свойства значение которого изменилось
-                OnPropertyChanged(nameof(CarId));
-            }
-        }
-        private string make;
-        public string Make
-        {
-            get => make;
-            set
-            {
-                if (value == make) return;
-                make = value;
-                OnPropertyChanged(nameof(Make));
-            }
-        }
-        private string color;
-        public string Color
-        {
-            get => color;
-            set
-            {
-                if (value == color) return;
-                color = value;
-                OnPropertyChanged(nameof(Color));
-            }
-        }
-        private string petName;
-        public string PetName
-        {
-            get => petName;
-            set
-            {
-                if (value == petName) return;
-                petName = value;
-                OnPropertyChanged(nameof(PetName));
-            }
-        }
-        
-        //флаг изменения модели
-        private bool isChanged;
-        public bool IsChanged
-        {
-            get => isChanged;
-            set
-            {
-                if (value == isChanged) return;
-                isChanged = value;
-                OnPropertyChanged(nameof(IsChanged));
-            }
-        }
-
+        public bool IsChanged { get; set; }
+        public int CarId { get; set; }
+        public string Make { get; set; }
+        public string Color { get; set; }
+        public string PetName { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary> Вспомогательный метод индицирования события изменения свойства </summary>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            if (propertyName != nameof(IsChanged))
-            {
-                IsChanged = true;
-            }
-            //индицирование события со строкой, указывающей свойство, которое было изменено и нуждается в обновлении
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            //обновление всех привязанных свойств объекта
-            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
-        }
     }
+    //class Inventory : INotifyPropertyChanged //наблюдаемая модель
+    //{
+    //    private int carId;
+    //    public int CarId
+    //    {
+    //        get => carId;
+    //        set
+    //        {
+    //            if (value == carId) return;
+    //            carId = value;
+    //            //передача названия свойства значение которого изменилось
+    //            OnPropertyChanged(nameof(CarId));
+    //        }
+    //    }
+    //    private string make;
+    //    public string Make
+    //    {
+    //        get => make;
+    //        set
+    //        {
+    //            if (value == make) return;
+    //            make = value;
+    //            OnPropertyChanged(nameof(Make));
+    //        }
+    //    }
+    //    private string color;
+    //    public string Color
+    //    {
+    //        get => color;
+    //        set
+    //        {
+    //            if (value == color) return;
+    //            color = value;
+    //            OnPropertyChanged(nameof(Color));
+    //        }
+    //    }
+    //    private string petName;
+    //    public string PetName
+    //    {
+    //        get => petName;
+    //        set
+    //        {
+    //            if (value == petName) return;
+    //            petName = value;
+    //            OnPropertyChanged(nameof(PetName));
+    //        }
+    //    }
+        
+    //    //флаг изменения модели
+    //    private bool isChanged;
+    //    public bool IsChanged
+    //    {
+    //        get => isChanged;
+    //        set
+    //        {
+    //            if (value == isChanged) return;
+    //            isChanged = value;
+    //            OnPropertyChanged(nameof(IsChanged));
+    //        }
+    //    }
+
+    //    public event PropertyChangedEventHandler PropertyChanged;
+
+    //    /// <summary> Вспомогательный метод индицирования события изменения свойства </summary>
+    //    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    //    {
+    //        if (propertyName != nameof(IsChanged))
+    //        {
+    //            IsChanged = true;
+    //        }
+    //        //индицирование события со строкой, указывающей свойство, которое было изменено и нуждается в обновлении
+    //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    //        //обновление всех привязанных свойств объекта
+    //        //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
+    //    }
+    //}
 }
