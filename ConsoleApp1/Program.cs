aa29bb108bb9e4981475f34ac5d6ad7f1083b070
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-22 01:18:13 +0300

    sample event

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,22 +7,64 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class MyNumber
+    class MyUser
     {
-        public int N { get; set; }
-        public MyNumber(int n) => N = n;
-        public static implicit operator int(MyNumber n) => n.N;
-        public override string ToString()
+        public string Name { get; set; }
+        public MyUser(string name)
         {
-            return $"Класс: {N}";
+            Name = name;
+        }
+        public void Say(string mes)
+        {
+            Console.WriteLine($"{Name} сказал {mes}");
+            eventSay?.Invoke(this, mes);
+        }
+        //событие - что-нить сказать
+        private Action<object, string> eventSay;
+        public event Action<object, string> EventSay
+        {
+            add
+            {
+                Console.WriteLine($"{Name} стали слушать");
+                eventSay += value;
+            }
+            remove
+            {
+                Console.WriteLine($"{Name} перестали слушать");
+                if (eventSay != null) eventSay -= value;
+            }
+        }
+        public void PrintHears()
+        {
+            foreach (var el in eventSay.GetInvocationList())
+            {
+                if (el.Target is MyUser user)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+        public void Hear(object obj, string mes)
+        {
+            var user = obj as MyUser;
+            Console.WriteLine($"Человек {Name} услышал >>> {user.Name} сказал {mes}");
         }
     }
+
     class Program
     {
         static void Main(string[] args)
         {
-            MyNumber n = new MyNumber(1);
-            Console.WriteLine(n);
+            MyUser user1 = new MyUser("Федя");
+            user1.Say("Привет мир!");
+            Console.WriteLine();
+            MyUser user2 = new MyUser("Петя");
+            user1.EventSay += user2.Hear;
+            user1.Say("Еще раз привет!");
+            MyUser user3 = new MyUser("Дима");
+            user1.EventSay += user3.Hear;
+            Console.WriteLine("\nСлушают первого:");
+            user1.PrintHears();
             Console.ReadKey();
         }
     }
