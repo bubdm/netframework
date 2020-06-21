d135895c0fd84cf0b67931be37036009d771d549
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-21 07:43:46 +0300

    ???

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    class MyNumber
+    {
+        public int N { get; set; }
+        public MyNumber(int n) => N = n;
+        public static implicit operator int(MyNumber n) => n.N;
+        public override string ToString()
+        {
+            return $"Класс: {N}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет мир!");
-            Console.WriteLine("Тест!");
+            MyNumber n = new MyNumber(1);
+            Console.WriteLine(n);
             Console.ReadKey();
         }
     }
