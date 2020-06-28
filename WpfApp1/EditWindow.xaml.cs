5cea9eae37cb7f999a2a763ca2b40ad2d1b9e922
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-28 08:30:30 +0300

    complete database sample

---
+++ WpfApp1/EditWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для EditWindow.xaml
+    /// </summary>
+    public partial class EditWindow : Window
+    {
+        private DataRow _dataRow;
+        public EditWindow(DataRow dataRow)
+        {
+            InitializeComponent();
+            _dataRow = dataRow;
+        }
+        private void EditWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            TextBoxFam.Text = _dataRow["fam"].ToString();
+            TextBoxName.Text = _dataRow["name"].ToString();
+            ButtonOk.Click += delegate
+            {
+                _dataRow["fam"] = TextBoxFam.Text;
+                _dataRow["name"] = TextBoxName.Text;
+                DialogResult = true;
+            };
+            ButtonCancel.Click += delegate { DialogResult = false; };
+        }
+    }
+}
---
+++ WpfApp1/SQLQueryCreateSampleDataBase.sql
@@ -0,0 +1,21 @@
+﻿--Создание таблиц с удаление уже имеющихся
+IF OBJECT_ID('Person' ) IS NOT NULL DROP TABLE Person;
+CREATE TABLE Person
+(
+id INT IDENTITY(1,1) NOT NULL,
+fam NVARCHAR(256) NOT NULL,
+name NVARCHAR(256) NOT NULL,
+CONSTRAINT PK_id_Employee PRIMARY KEY (id),
+);
+--Тестовые данные
+DECLARE @count_persons INT = 1000;
+DECLARE @i INT = 1;
+WHILE @i <= @count_persons
+BEGIN
+	INSERT INTO Person(fam, name)
+	VALUES (CONCAT(N'Тестов', @i),CONCAT(N'Тест', @i));
+	SET @i = @i + 1;
+END
+--Сформированные тестовые данные
+SELECT id,fam,name FROM Person;
+
