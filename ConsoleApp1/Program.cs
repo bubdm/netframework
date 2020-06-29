8c22e94a0753effd70af24a63aee3c45ab39be4a
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-29 23:30:48 +0300

    clear

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
@@ -10,42 +11,13 @@ using static System.Console;
 
 namespace ConsoleApp1
 {
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            string dataProvider = ConfigurationManager.AppSettings["provider"];
-            string connectionString = ConfigurationManager.AppSettings["connectionString"];
 
-            DbProviderFactory factory = DbProviderFactories.GetFactory(dataProvider);
-            using (DbConnection connection = factory.CreateConnection())
-            {
-                if (connection == null)
-                {
-                    WriteLine("Ошибка создания объекта Connection");
-                    ReadKey();
-                    return;
-                }
-                connection.ConnectionString = connectionString;
-                connection.Open();
-                DbCommand command = factory.CreateCommand();
-                if (command == null)
-                {
-                    WriteLine("Ошибка создания объекта Command");
-                    ReadKey();
-                    return;
-                }
-                command.Connection = connection;
-                command.CommandText = "SELECT * FROM Inventory";
-                using (DbDataReader dataReader = command.ExecuteReader())
-                {
-                    WriteLine("Таблица Inventory:");
-                    while (dataReader.Read())
-                        WriteLine($"-> Автомобиль {dataReader["CarId"]} это {dataReader["Make"]}.");
-                }
-            }
+
+
             ReadKey();
         }
     }
