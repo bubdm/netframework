01e94bfaab2e31497de71bfd1a6622fba5f85e70
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-07-01 09:55:15 +0300

    create library

--- AutoLotDAL/DataOperations/InventoryDAL.cs
+++ AutoLotDAL/DataOperations/InventoryDAL.cs
@@ -12,7 +12,7 @@ namespace AutoLotDAL.DataOperations
     public class InventoryDAL
     {
         private readonly string _connectionString;
-        public InventoryDAL() : this(@"Data Source=PNZASUTP\SQLEXPRESS;Initial Catalog=AutoLot;Integrated Security=True") { }
+        public InventoryDAL() : this(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AutoLot;Integrated Security=True") { }
         public InventoryDAL(string connectionString)
         {
             _connectionString = connectionString;
@@ -28,6 +28,7 @@ namespace AutoLotDAL.DataOperations
             if (_sqlConnection?.State != ConnectionState.Closed)
                 _sqlConnection.Close();
         }
+        /// <summary> Все автомобили </summary>
         public List<Car> GetAllInventory()
         {
             OpenConnection();
@@ -41,13 +42,131 @@ namespace AutoLotDAL.DataOperations
                     inventory.Add(new Car
                     {
                         CarId = (int)reader["CarId"],
-
-
+                        Make = (string)reader["Make"],
+                        Color = (string)reader["Color"],
+                        Name = (string)reader["Name"],
                     });
                 }
+                reader.Close();
             }
             return inventory;
         }
-
+        /// <summary> Один автомобиль </summary>
+        public Car GetCar(int id)
+        {
+            OpenConnection();
+            Car car = null;
+            string sql = $"SELECT * FROM Inventory WHERE CarId = @id";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.Parameters.Add("@id", SqlDbType.Int, -1).Value = id;
+                SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                while (reader.Read())
+                {
+                    car = new Car
+                    {
+                        CarId = (int)reader["CarId"],
+                        Make = (string)reader["Make"],
+                        Color = (string)reader["Color"],
+                        Name = (string)reader["Name"],
+                    };
+                }
+                reader.Close();
+            }
+            return car;
+        }
+        /// <summary> Добавление автомобиля </summary>
+        public void InsertCar(string make, string color, string name)
+        {
+            OpenConnection();
+            string sql = $"INSERT INTO Inventory (Make, Color, Name) VALUES (@Make, @Color, @Name)";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.Parameters.Add("@Make", SqlDbType.NVarChar, 50).Value = make;
+                command.Parameters.Add("@Color", SqlDbType.NVarChar, 50).Value = color;
+                command.Parameters.Add("@Name", SqlDbType.NVarChar, 50).Value = name;
+                command.ExecuteNonQuery();
+            }
+            CloseConnection();
+        }
+        /// <summary> Добавление автомобиля </summary>
+        public void InsertCar(Car car)
+        {
+            OpenConnection();
+            string sql = $"INSERT INTO Inventory (Make, Color, Name) VALUES (@Make, @Color, @Name)";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.Parameters.Add("@Make", SqlDbType.NVarChar, 50).Value = car.Make;
+                command.Parameters.Add("@Color", SqlDbType.NVarChar, 50).Value = car.Color;
+                command.Parameters.Add("@Name", SqlDbType.NVarChar, 50).Value = car.Name;
+                command.ExecuteNonQuery();
+            }
+            CloseConnection();
+        }
+        /// <summary> Обновление имени автомобиля </summary>
+        /// <param name="id"></param>
+        public void UpdateCarName(int id, string newName)
+        {
+            OpenConnection();
+            string sql = $"UPDATE Inventory SET Name = @Name WHERE CarId = @id";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.Parameters.Add("@Name", SqlDbType.NVarChar, 50).Value = newName;
+                command.Parameters.Add("@id", SqlDbType.Int, -1).Value = id;
+                command.ExecuteNonQuery();
+            }
+            CloseConnection();
+        }
+        /// <summary> Удаление автомобиля </summary>
+        public int DeleteCar(int id)
+        {
+            int retMe;
+            OpenConnection();
+            string sql = $"DELETE FROM Inventory WHERE CarId = @id";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.Parameters.Add("@id", SqlDbType.Int, -1).Value = id;
+                try
+                {
+                    retMe = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new ApplicationException("Этот автомобиль уже заказан!", ex);
+                }
+            }
+            CloseConnection();
+            return retMe;
+        }
+        /// <summary> Получение имени автомобиля </summary>
+        public string GetCarName(int carId)
+        {
+            OpenConnection();
+            string carName = default;
+            using (SqlCommand command = new SqlCommand("GetPetsName", _sqlConnection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter parameter = new SqlParameter
+                {
+                    ParameterName = "@carID",
+                    SqlDbType = SqlDbType.Int,
+                    Value = carId,
+                    Direction = ParameterDirection.Input,
+                };
+                command.Parameters.Add(parameter);
+                parameter = new SqlParameter
+                {
+                    ParameterName = "@name",
+                    SqlDbType = SqlDbType.Char,
+                    Size = 10,
+                    Direction = ParameterDirection.Output,
+                };
+                command.Parameters.Add(parameter);
+                command.ExecuteNonQuery();
+                carName = (string)command.Parameters["@name"].Value;
+                CloseConnection();
+            }
+            return carName;
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.Linq;
+using AutoLotDAL.DataOperations;
+using AutoLotDAL.Modes;
 
 namespace ConsoleApp1
 {
@@ -7,29 +10,46 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            SqlConnectionStringBuilder sqlStringBuilder = new SqlConnectionStringBuilder
+            InventoryDAL inventory = new InventoryDAL();
+            Console.WriteLine("Все:");
+            var list = inventory.GetAllInventory();
+            Console.WriteLine("Ид\tМарка\tЦвет\tНазвание");
+            foreach(var el in list)
+                Console.WriteLine($"{el.CarId}\t{el.Make}\t{el.Color}\t{el.Name}");
+            Console.WriteLine();
+            try
             {
-                InitialCatalog = "autolot",
-                DataSource = @"PNZASUTP\SQLEXPRESS",
-                ConnectTimeout = 30,
-                IntegratedSecurity = true,
-            };
-            using (SqlConnection connection = new SqlConnection())
-            {
-                connection.ConnectionString = sqlStringBuilder.ConnectionString;
-                connection.Open();
-                SqlCommand command = new SqlCommand("SELECT * FROM Inventory", connection);
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        Console.WriteLine("Запись:");
-                        for (int i = 0; i < reader.FieldCount; i++)
-                            Console.WriteLine($"{reader.GetName(i)} = {reader.GetValue(i)}");
-                        Console.WriteLine();
-                    }
-                }
+                var count = inventory.DeleteCar(5);
+                if (count != 0)
+                    Console.WriteLine("Автомобиль удален");
+                else
+                    Console.WriteLine("Автомобиль не удален, так как не найден!");
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Автомобиль не удален");
+                Console.WriteLine(ex.Message);
+            }
+            var car = inventory.GetCar(1);
+            Console.WriteLine($"Один автомобиль: {car.Make} {car.Name} {car.Color}");
+            inventory.InsertCar(new Car {Make = "One", Color = "Gray", Name = "Test"});
+            list = inventory.GetAllInventory();
+            var newCar = list.First(l => l.Name == "Test").CarId;
+            inventory.UpdateCarName(newCar, "Новое имя");
+            Console.WriteLine("После добавления и изменения:");
+            list = inventory.GetAllInventory();
+            Console.WriteLine("Ид\tМарка\tЦвет\tНазвание");
+            foreach(var el in list)
+                Console.WriteLine($"{el.CarId}\t{el.Make}\t{el.Color}\t{el.Name}");
+            Console.WriteLine();
+            var Name = inventory.GetCarName(newCar);
+            Console.WriteLine($"Имя автомобиля: {Name}");
+            inventory.DeleteCar(newCar);
+            Console.WriteLine("После удаления:");
+            list = inventory.GetAllInventory(); 
+            Console.WriteLine("Ид\tМарка\tЦвет\tНазвание");
+            foreach(var el in list)
+                Console.WriteLine($"{el.CarId}\t{el.Make}\t{el.Color}\t{el.Name}");
             Console.ReadKey();
         }
     }
