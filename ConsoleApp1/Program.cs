a3278b747e1cdba0c59a9de8a66392bf8c4a6a52
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-07-07 19:36:43 +0300

    clear console

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -12,13 +11,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Database.SetInitializer(new MyDataInitializer());
-            Console.WriteLine("Список:");
-            using (var context = new AutoLotEntities())
-            {
-                foreach (var el in context.Inventories)
-                    Console.WriteLine(el);
-            }
+
+
             Console.WriteLine("Нажмите любую кнопку ...");
             Console.ReadKey();
         }
