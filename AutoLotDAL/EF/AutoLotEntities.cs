85eb860a73c893e184d525de405033f30ed4ce37
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-07-09 22:00:24 +0300

    add interceptor

--- AutoLotDAL/EF/AutoLotEntities.cs
+++ AutoLotDAL/EF/AutoLotEntities.cs
@@ -1,12 +1,15 @@
+using System.Data.Entity.Infrastructure.Interception;
+using AutoLotDAL.Interception;
+
 namespace AutoLotDAL.EF
 {
     using System.Data.Entity;
-
     public partial class AutoLotEntities : DbContext
     {
         public AutoLotEntities()
             : base("name=AutoLotConnection")
         {
+            DbInterception.Add(new ConsoleWriterInterceptor());
         }
         public virtual DbSet<CreditRisk> CreditRisks { get; set; }
         public virtual DbSet<Customer> Customers { get; set; }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
 using AutoLotDAL.EF;
 using AutoLotDAL.Repos;
 
@@ -15,9 +17,38 @@ namespace ConsoleApp1
                     Console.WriteLine(el);
                 }
             }
+            TestConcurrency();
             Console.WriteLine("Нажмите любую кнопку ...");
             Console.ReadKey();
         }
+
+        /// <summary> Тест параллелизма </summary>
+        private static void TestConcurrency()
+        {
+            var repo1 = new InventoryRepo();
+            var repo2 = new InventoryRepo();
+            var car1 = repo1.GetOne(1);
+            var car2 = repo2.GetOne(1);
+            car1.Name = "NewName";
+            repo1.Save(car1);
+            car2.Name = "OtherName";
+            try
+            {
+                repo2.Save(car2);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                var entry = ex.Entries.Single();
+                var current = entry.CurrentValues;
+                var original = entry.OriginalValues;
+                var baseVal = entry.GetDatabaseValues();
+                Console.WriteLine("Exception");
+                Console.WriteLine($"Current: {current[nameof(Inventory.Name)]}");
+                Console.WriteLine($"Orig: {original[nameof(Inventory.Name)]}");
+                Console.WriteLine($"base: {baseVal[nameof(Inventory.Name)]}");
+            }
+        }
+
         /// <summary> Добавление </summary>
         private static void AddNewRecord(Inventory inventory)
         {
