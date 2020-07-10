bb3a91f3283049514dc390a095bde29b92cec0ba
kanadeiar <kanadeiar@gmail.com>
2020-07-10 10:45:55 +0300

    add 2 interceptors

--- AutoLotDAL/EF/AutoLotEntities.cs
+++ AutoLotDAL/EF/AutoLotEntities.cs
@@ -3,13 +3,51 @@ using AutoLotDAL.Interception;
 
 namespace AutoLotDAL.EF
 {
+    using System;
     using System.Data.Entity;
+    using System.Data.Entity.Core.Objects;
+    using System.Data.Entity.Infrastructure;
+
     public partial class AutoLotEntities : DbContext
     {
+        static readonly DatabaseLogger DatabaseLogger = new DatabaseLogger("sqllog.txt", true);
         public AutoLotEntities()
             : base("name=AutoLotConnection")
         {
-            DbInterception.Add(new ConsoleWriterInterceptor());
+            //DbInterception.Add(new ConsoleWriterInterceptor());
+            //DatabaseLogger.StartLogging();
+            //DbInterception.Add(DatabaseLogger);
+            var context = (this as IObjectContextAdapter).ObjectContext;
+            context.ObjectMaterialized += OnObjectMaterialized;
+            context.SavingChanges += OnSavingChanges;
+        }
+        private void OnSavingChanges(object sender, EventArgs eventArgs)
+        { //текущие, исходные значения, можно отменять/модифицировать операции сохранения любым способом
+            var context = sender as ObjectContext;
+            if (context == null) return;
+            foreach (var item in context.ObjectStateManager.GetObjectStateEntries(EntityState.Modified | EntityState.Added))
+            { //добавленные или измененнные
+                if ((item.Entity as Inventory) != null)
+                {
+                    var entity = (Inventory)item.Entity;
+                    if (entity.Name == "NewName")
+                    { //отклоняет все изменения названий в Запорожец
+                        item.RejectPropertyChanges(nameof(entity.Name));
+                    }
+                }
+            }
+        }
+        private void OnObjectMaterialized(object sender, System.Data.Entity.Core.Objects.ObjectMaterializedEventArgs e)
+        { //полезно при работе с WPF
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            //DbInterception.Remove(DatabaseLogger);
+            //DatabaseLogger.StopLogging();
+            base.Dispose(disposing);
         }
         public virtual DbSet<CreditRisk> CreditRisks { get; set; }
         public virtual DbSet<Customer> Customers { get; set; }
