1a963f7e0cf5ab80b5dccf98de393abaf93851b4
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-07-13 19:41:54 +0300

    indexator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,11 +8,56 @@ namespace ConsoleApp1
 {
     class Program
     {
+        class MySuperPuperArray
+        {
+            private int[] arr1; //приватный массив
+            private int[] arr2; //приватный массив
+            private int[] arr3; //приватный массив
+            public MySuperPuperArray(int size)
+            {
+                arr1 = new int[size];
+                arr2 = new int[size];
+                arr3 = new int[size];
+            }
+            /// <summary> Индексатор-матрешка </summary>
+            /// <param name="i">номер элемента</param>
+            /// <param name="number">номер массива</param>
+            /// <returns>элемент</returns>
+            public int this[int i, int number]
+            {
+                get
+                {
+                    switch (number)
+                    {
+                        case 1: return arr1[i];
+                        case 2: return arr2[i];
+                        case 3: return arr3[i];
+                        default: throw new ArgumentOutOfRangeException("Не делайте так!!!");
+                    }
+                }
+                set
+                {
+                    switch (number)
+                    {
+                        case 1: arr1[i] = value; break;
+                        case 2: arr2[i] = value; break;
+                        case 3: arr3[i] = value; break;
+                        default: throw new ArgumentOutOfRangeException("Никода не делайте!!!");
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет мир!");
+            var mySuper = new MySuperPuperArray(10);
+            for (int i = 0; i < 10; i++)
+            {
+                mySuper[i, 1] = i;
+            }
             Console.WriteLine("Тест!");
             Console.ReadKey();
         }
+
+
     }
 }
