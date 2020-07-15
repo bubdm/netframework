ccc4057eb081fe7b642bfaad56faead5791bebf4
kanadeiar <kanadeiar@gmail.com>
2020-07-15 15:38:20 +0300

    add dataset

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,10 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
+            DataSet dataSet = new DataSet();
+            
+
+
 
 
             Console.WriteLine("Нажмите любую кнопку ...");
