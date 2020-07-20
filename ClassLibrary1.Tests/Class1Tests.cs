f8942919625bb8aadec075188f3fd35fa48435bc
kanadeiar <kanadeiar@gmail.com>
2020-07-20 20:45:31 +0300

    clean tests

--- ClassLibrary1.Tests/Class1Tests.cs
+++ ClassLibrary1.Tests/Class1Tests.cs
@@ -7,48 +7,6 @@ namespace ClassLibrary1.Tests
     [TestClass]
     public class Class1Tests
     {
-        public TestContext TestContext { get; set; }
-        private Class1 class1 = new Class1();
 
-        [DataTestMethod]
-        [DataRow("Andrei", 18)]
-        [DataRow("Maxim", 20)]
-        [DataRow("Xianon", 22)]
-        public void GoodDataTest_Data_Trues(string name, int age)
-        {
-            Assert.IsTrue(class1.Good(name, age));
-        }
-
-        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
-            "testData.xml",
-            "User",
-            DataAccessMethod.Sequential)]
-        public void Good_FromXML_Trues()
-        {
-            string name = Convert.ToString(TestContext.DataRow["name"]);
-            int age = Convert.ToInt32(TestContext.DataRow["age"]);
-
-            bool result = class1.Good(name, age);
-
-            Assert.IsTrue(result);
-        }
-
-
-
-
-        [TestMethod]
-        [DeploymentItem("Files\\ExamCreatedResult.txt")]
-        [DeploymentItem("Files\\ExamCreatedTemplate.txt")]
-        public void FromTemplate_PassTestValue_StringFromTemplateReturned()
-        {
-            Class1 class0 = new Class1();
-            class0.TemplateFolder = "."; //текущая директория
-
-            string expected = File.ReadAllText("ExamCreatedResult.txt");
-
-            string actual = class0.FromTemplate("TestExam", "Beginer");
-
-            Assert.AreEqual(expected, actual);
-        }
     }
 }
--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -9,37 +9,6 @@ namespace ClassLibrary1
 {
     public class Class1
     {
-        public static double GetSqrt(double x)
-        {
-            return Math.Sqrt(x);
-        }
-        public string SayHello(string name)
-        {
-            if (name == null)
-                throw new ArgumentNullException("Not Parameter");
-            return "Hello " + name;
-        }
 
-        public bool Good(string name, int age)
-        {
-            if (name.Length < 3)
-                return false;
-            if (age <= 0)
-                return true;
-            return true;
-        }
-
-
-
-        public string TemplateFolder { get; set; }
-        public string FromTemplate(string name, string level)
-        {
-            string path = Path.Combine(TemplateFolder, "ExamCreatedTemplate.txt");
-            string template = File.ReadAllText(path);
-            template = template.Replace("{Name}", name);
-            template = template.Replace("{Level}", level);
-
-            return template;
-        }
     }
 }
