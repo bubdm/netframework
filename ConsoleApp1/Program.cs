ab46b06f68444475b9b53747fa639a6d51991305
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-09-20 14:55:38 +0300

    work application

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +13,27 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Привет мир!");
-            Console.WriteLine("Тест!");
+            
+            MailMessage mm = new MailMessage("kanadeiar@gmail.com","kanadei@mail.ru");
+            mm.Subject = "Заголовок окна";
+            mm.Body = "Содержимое письма";
+            mm.IsBodyHtml = false;
+
+            //https://myaccount.google.com/lesssecureapps
+            SmtpClient sc = new SmtpClient("smtp.gmail.com",587);
+            sc.EnableSsl = true;
+            sc.Credentials = new NetworkCredential("kanadeiar@gmail.com", "12Am52205198874");
+            try
+            {
+                sc.Send(mm);
+                Console.WriteLine("Письмо отправлено!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+
             Console.ReadKey();
         }
     }
