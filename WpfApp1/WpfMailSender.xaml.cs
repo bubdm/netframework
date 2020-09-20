38278fde1f12adc99b609f047d25e90ecb9924b6
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-09-20 17:21:12 +0300

    работает отправка писем

---
+++ WpfApp1/WpfMailSender.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Xps.Packaging;
+using Microsoft.Win32;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для WpfMailSender.xaml
+    /// </summary>
+    public partial class WpfMailSender : Window
+    {
+
+        
+        public WpfMailSender()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ButtonSendMail_OnClick(object sender, RoutedEventArgs e)
+        {
+            List<string> listMails = new List<string> {"kanadei@mail.ru", "kanadei@yandex.ru"};
+            string password = PasswordBox.Password;
+            foreach (var mail in listMails)
+            {
+                using (MailMessage mm = new MailMessage("kanadeiar@gmail.com", mail))
+                {
+                    mm.Subject = "Привет из C#";
+                    mm.Body = "Привет!";
+                    mm.IsBodyHtml = false;
+                    using (SmtpClient sc = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        sc.EnableSsl = true;
+                        sc.Credentials = new NetworkCredential("kanadeiar@gmail.com", password);
+                        try
+                        {
+                            sc.Send(mm);
+                            MessageBox.Show("Письмо успешно отправлено.");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Не удалось отправить!\n" + ex.ToString());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
